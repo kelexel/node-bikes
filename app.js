27ef5d326415997e67f7b67e297711ae43731b50
Rud <rudolph.sand@gmail.com>
2013-03-17 11:36:27 +0100

    cookie fix#2

--- app.js
+++ app.js
@@ -25,10 +25,10 @@ global.logger = logger;
 
 // CORS settings, passing * for now
 app.all('*', function(req, res, next){
-	res.set('Access-Control-Allow-Credentials', true);
 	if (!req.get('Origin')) return next();
 	// use "*" here to accept any origin
 	res.set('Access-Control-Allow-Origin', '*');
+	res.set('Access-Control-Allow-Credentials', true);
 	res.set('Access-Control-Allow-Methods', 'GET, POST');
 	res.set('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type');
 	// res.set('Access-Control-Allow-Max-Age', 3600);
